245a64f85916113e1fbbd52d0017754d184c0d9a
m-gheini <mahya.gheini78@gmail.com>
2021-12-21 22:17:54 +0330

    Update handling input + Adding Network class + Calculate Coefficients

--- backLogic.py
+++ backLogic.py
@@ -2,9 +2,10 @@ import os
 import pandas as pd
 import networkx as nx
 from shocks import Shock
+from network import Network
 from shockManager import ShockManager
 
-infoDict = {'inputFile': './Assets/ICIO2018_2015.CSV', 'outputName': 'res', 'outputPath': 'C:/', 'imCountry': 'CAN',
+info = {'inputFile': './Assets/ICIO2018_2015.CSV', 'outputName': 'res', 'outputPath': 'C:/', 'imCountry': 'CAN',
             'imSector': '07T08', 'exCountry': 'BEL', 'exSector': '09', 'shockSrc': 'importer', 'shockSign': '-',
             'shockAmount': '4', 'shockTo': 'intermediate goods', 'shockItr': '11', 'shockThr': 'NOT CHOSEN',
             'imScenario': 'option 1', 'exScenario': 'option 4', 'imAlter': 'NONE', 'exAlter': 'EST_19 : 100'}
@@ -49,46 +50,67 @@ def getColNumOfFinalDemand(dataframe):
             return j
 
 
-def getImExAdjacency(dataframe):
+def getZ(dataframe):
     row = getRowNumOfFirstTax(dataframe)
     col = getColNumOfFinalDemand(dataframe)
-    adjacencyDF = dataframe.iloc[:row, :col]
-    twoLastRow = dataframe.iloc[len(getFirstColumn(dataframe)) - 2:, :col]
-    adjacencyDF = adjacencyDF.append(twoLastRow)
-    adjacencyDF.index = range(adjacencyDF.shape[0])
-    return adjacencyDF
+    z = dataframe.iloc[1:row, 1:col]
+    z = z.astype(float)
+    return z
 
 
-def prepareNetworkUsingLibrary(dataframe):
-    network = nx.DiGraph()
-    for i in range(1, len(getFirstRow(dataframe))):
-        for j in range(1, len(getFirstColumn(dataframe)) - 2):
-            if float(dataframe[i][j]) != 0:
-                network.add_edge(dataframe[i][0], dataframe[0][j], weight=float(dataframe[i][j]))
-    return network
+def getX(dataframe):
+    col = getColNumOfFinalDemand(dataframe)
+    x = dataframe.iloc[len(getFirstColumn(dataframe)) - 1:, 1:col]
+    x = x.astype(float)
+    return x
 
 
-def getProvidersForSector(graph, node):
-    return list(graph.successors(node))
+def getAddedValue(dataframe):
+    col = getColNumOfFinalDemand(dataframe)
+    addedValue = dataframe.iloc[len(getFirstColumn(dataframe)) - 2:len(getFirstColumn(dataframe)) - 1, 1:col]
+    addedValue = addedValue.astype(float)
+    return addedValue
+
+# def getImExAdjacency(dataframe):
+#     row = getRowNumOfFirstTax(dataframe)
+#     col = getColNumOfFinalDemand(dataframe)
+#     adjacencyDF = dataframe.iloc[:row, :col]
+#     twoLastRow = dataframe.iloc[len(getFirstColumn(dataframe)) - 2:, :col]
+#     adjacencyDF = adjacencyDF.append(twoLastRow)
+#     adjacencyDF.index = range(adjacencyDF.shape[0])
+#     return adjacencyDF
 
 
-def getDemandersFromSector(graph, node):
-    return list(graph.predecessors(node))
-
-
-def updateEdgeWeight(graph, edge, newWeight):
-    demander, provider = edge
-    graph[demander][provider]["weight"] = newWeight
-    return graph
-
-
-def prepareName(country, sector):
-    return country + "_" + sector
-
-
-def makeShockObject(origin, destination, amount, sign, iteration):
-    shock = Shock(origin, destination, amount, sign, iteration)
-    return shock
+# def prepareNetworkUsingLibrary(dataframe):
+#     network = nx.DiGraph()
+#     for i in range(1, len(getFirstRow(dataframe))):
+#         for j in range(1, len(getFirstColumn(dataframe)) - 2):
+#             if float(dataframe[i][j]) != 0:
+#                 network.add_edge(dataframe[i][0], dataframe[0][j], weight=float(dataframe[i][j]))
+#     return network
+#
+#
+# def getProvidersForSector(graph, node):
+#     return list(graph.successors(node))
+#
+#
+# def getDemandersFromSector(graph, node):
+#     return list(graph.predecessors(node))
+#
+#
+# def updateEdgeWeight(graph, edge, newWeight):
+#     demander, provider = edge
+#     graph[demander][provider]["weight"] = newWeight
+#     return graph
+#
+#
+# def prepareName(country, sector):
+#     return country + "_" + sector
+#
+#
+# def makeShockObject(origin, destination, amount, sign, iteration):
+#     shock = Shock(origin, destination, amount, sign, iteration)
+#     return shock
 
 
 def main(data):
@@ -97,21 +119,27 @@ def main(data):
     infoDict = data
     print("IN BACK")
     df = readData(infoDict["inputFile"])
-    adjacencyDF = getImExAdjacency(df)
-    network = prepareNetworkUsingLibrary(adjacencyDF)
-    print(len(list(network.edges)))
-    if infoDict["shockSrc"] == "importer":
-        origin = prepareName(infoDict["imCountry"], infoDict["imSector"])
-        destination = prepareName(infoDict["exCountry"], infoDict["exSector"])
-    elif infoDict["shockSrc"] == "exporter":
-        origin = prepareName(infoDict["exCountry"], infoDict["exSector"])
-        destination = prepareName(infoDict["imCountry"], infoDict["imSector"])
-    makeShockObject(origin, destination, infoDict["shockAmount"], infoDict["shockSign"], 1)
-    if infoDict["shockItr"] == "NOT CHOSEN":
-        thr = int(infoDict["shockThr"])
-    elif infoDict["shockThr"] == "NOT CHOSEN":
-        itr = int(infoDict["shockItr"])
-    ShockManager(network, itr, thr)
+    Z = getZ(df)
+    X = getX(df)
+    header = list(getFirstRow(Z))[1:]
+    network = Network(Z, X, header)
+    print(network.Header)
+    Z.div(X.iloc[0] + 0.000000001)
+    # adjacencyDF = getImExAdjacency(df)
+    # network = prepareNetworkUsingLibrary(adjacencyDF)
+    # print(len(list(network.edges)))
+    # if infoDict["shockSrc"] == "importer":
+    #     origin = prepareName(infoDict["imCountry"], infoDict["imSector"])
+    #     destination = prepareName(infoDict["exCountry"], infoDict["exSector"])
+    # elif infoDict["shockSrc"] == "exporter":
+    #     origin = prepareName(infoDict["exCountry"], infoDict["exSector"])
+    #     destination = prepareName(infoDict["imCountry"], infoDict["imSector"])
+    # makeShockObject(origin, destination, infoDict["shockAmount"], infoDict["shockSign"], 1)
+    # if infoDict["shockItr"] == "NOT CHOSEN":
+    #     thr = int(infoDict["shockThr"])
+    # elif infoDict["shockThr"] == "NOT CHOSEN":
+    #     itr = int(infoDict["shockItr"])
+    # ShockManager(network, thr, itr)
 
 
-main(infoDict)
+main(info)
---
+++ network.py
@@ -0,0 +1,135 @@
+class Network:
+    def __init__(self, Z, X, Header):
+
+        self.Z = Z
+        self.X = X
+        self.Header = Header
+        # self.A0 = getLeontiefCoefficients(self.A, self.X)
+        # self.A_Inverse = getLeontiefInverseMatrix(self.A0)
+        # self.graph = {}
+        # self.internalQueue = dict()
+        # self.shockQueue = dict()
+        #
+        # logger.info(f"A:\n{self.A}")
+        # logger.info(f"X:\n{self.X}")
+        # logger.info(f"Header:\n{self.Header}")
+        # logger.info(f"\nTechnical Coeff:\n{self.A0}")
+        # logger.info(f"\nA0 Inverse:\n{self.A_Inverse}")
+
+        # self.generateStructue()
+    #
+    # def generateStructue(self):
+    #     logger.info("-=-" * 10)
+    #     logger.info("Generating Graph Structure ...")
+    #     logger.info("-=-" * 10)
+    #     for i, node in enumerate(self.Header):
+    #         newNode = Node(node)
+    #         provides_to = []
+    #         demands_from = []
+    #         provides_to_edges = []
+    #         demands_from_edges = []
+    #         for j, _ in enumerate(self.Header):
+    #             if i != j and self.A[i][j] != 0:
+    #                 provides_to.append(self.Header[j])
+    #                 provides_to_edges.append(Edge(self.Header[i], self.Header[j], self.A[i][j], "Provider"))
+    #             if i != j and self.A[j][i] != 0:
+    #                 demands_from.append(self.Header[j])
+    #                 demands_from_edges.append(Edge(self.Header[i], self.Header[j], self.A[j][i], "Demander"))
+    #         newNode.demands_from = demands_from
+    #         newNode.provides_to = provides_to
+    #         newNode.demands_from_edges = demands_from_edges
+    #         newNode.provides_to_edges = provides_to_edges
+    #
+    #         self.graph[node] = newNode
+    #
+    #         logger.info("New Node Added ...")
+    #         logger.info(pformat(newNode))
+    #
+    #     logger.info("-=-" * 10)
+    #     logger.info(f"Final Graph Structure : \n{pformat(self.graph)}")
+    #     logger.info("-=-" * 10)
+    #     self.applyShock()
+    #
+    # def getIndex(self, node_name):
+    #     index = -1
+    #     for i, name in enumerate(self.Header):
+    #         if name == node_name:
+    #             index = i
+    #             break
+    #     if index >= 0:
+    #         return index
+    #     raise Exception("Index Not Found in Header Names")
+    #
+    # def test_shock(self):
+    #     import random
+    #     for node, node_data in self.graph.items():
+    #         for edge in node_data.provides_to_edges_info:
+    #             edge.update_weight(random.randint(1, 30))
+    #
+    #     for node, node_data in self.graph.items():
+    #         for edge in node_data.provides_to_edges_info:
+    #             logger.info("=-" * 25)
+    #             logger.info(pformat(edge))
+    #             logger.info("=-" * 25)
+    #             logger.info(
+    #                 f"Edge  History:\n <{edge.nodes[0]},{edge.nodes[1]}> - Changes Over Time:\n{pformat(edge.get_history())}")
+    #
+    # def applyShock(self, deltaF=None):
+    #     deltaF = [0, 20, 0]
+    #     logger.info("-=-" * 10)
+    #     logger.info(f"Delta F : {deltaF}")
+    #     logger.info("-=-" * 10)
+    #     iteration = 0
+    #     flag = True
+    #
+    #     self.internalQueue[iteration] = dict()
+    #     for i, shock in enumerate(deltaF):
+    #         if shock != 0:
+    #             logger.info(f"Shock Originated From : {self.Header[i]} ")
+    #             for target in self.graph[self.Header[i]].demands_from:
+    #                 target_index = self.getIndex(target)
+    #                 logger.info(f"  Target of Shock : {target}")
+    #                 logger.info(f"    Received Shock : {shock}")
+    #                 logger.info(f"    Coefficient Applied : {self.A0[target_index][i]}")
+    #                 self.internalQueue[iteration][target] = {
+    #                     self.Header[i]: shock * self.A0[target_index][i]
+    #                 }
+    #             logger.info("-=-" * 10)
+    #     self.__processInternalQueue__(0)
+    #     iteration += 1
+    #     while iteration < 10 and flag:
+    #         self.internalQueue[iteration] = dict()
+    #         deltaF = [0, 0, 0]
+    #         for target_node, delta_f in self.shockQueue[iteration - 1].items():
+    #             deltaF[self.getIndex(target_node)] = delta_f
+    #         logger.info("-=-" * 10)
+    #         logger.info(f"Delta F : {deltaF}")
+    #         logger.info("-=-" * 10)
+    #         for i, shock in enumerate(deltaF):
+    #             if shock != 0:
+    #                 logger.info(f"Shock Originated From : {self.Header[i]} ")
+    #                 for target in self.graph[self.Header[i]].demands_from:
+    #                     target_index = self.getIndex(target)
+    #                     logger.info(f"  Target of Shock : {target}")
+    #                     logger.info(f"    Received Shock : {shock}")
+    #                     logger.info(f"    Coefficient Applied : {self.A0[target_index][i]}")
+    #                     self.internalQueue[iteration][target] = {
+    #                         self.Header[i]: shock * self.A0[target_index][i]
+    #                     }
+    #                 logger.info("-=-" * 10)
+    #         self.__processInternalQueue__(iteration)
+    #         iteration += 1
+    #
+    # def __processInternalQueue__(self, iteration):
+    #     logger.info(f"Processing Queue - Iteration : {iteration}")
+    #     logger.info(f"Internal Queue : \n{pformat(self.internalQueue)}")
+    #     logger.info("-=-" * 10)
+    #     # key = f'{iteration}'
+    #     self.shockQueue[iteration] = dict()
+    #     for target, shocks in self.internalQueue[iteration].items():
+    #         s = 0
+    #         for shock in shocks.values():
+    #             s += shock
+    #         self.shockQueue[iteration][target] = s
+    #     logger.info(f"Shock Queue :\n{self.shockQueue}")
+    #     logger.info("-=-" * 10)
